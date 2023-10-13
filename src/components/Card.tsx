9dbef474c84e7b1db35fc9731b1762f0ca5d4fd5
Emre Neumann <emre.neumann@crownpeak.com>
2023-10-13 20:51:45 +0200

    fixed background colors ... again

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,63 +8,61 @@ type CardProps = {
   dark?: boolean;
 };
 
+//doing this the really dumb way because tailwind wants full class names somewhere in the code
 function getCSSColorByType(colortype: string, dark: boolean | undefined) {
   let cssColor = "";
-  switch (colortype) {
-    case "Red":
-      cssColor += "bg-red-";
-      break;
-    case "Blue":
-      cssColor += "bg-blue-";
-      break;
-    case "Green":
-      cssColor += "bg-green-";
-      break;
-    case "Black":
-      cssColor += "bg-black-";
-      break;
-    case "White":
-      cssColor += "bg-cream-";
-      break;
-    case "Artifact":
-      cssColor += "bg-brown-";
-      break;
-    case "Land":
-      cssColor += "bg-land-";
-      break;
-    case "Multicolor":
-      cssColor += "bg-gold-";
-      break;
-    default:
-      cssColor += "bg-white";
-      break;
-    }
-    if(cssColor !== "bg-white"){
-      cssColor += dark ? "500" : "200";
+    switch (colortype) {
+      case "Red":
+        cssColor += dark ? "bg-red-500" : "bg-red-200";
+        break;
+      case "Blue":
+        cssColor += dark ? "bg-blue-500" : "bg-blue-200";
+        break;
+      case "Green":
+        cssColor += dark ? "bg-green-500" : "bg-green-200";
+        break;
+      case "Black":
+        cssColor += dark ? "bg-black-500" : "bg-black-200";
+        break;
+      case "White":
+        cssColor += dark ? "bg-cream-500" : "bg-cream-200";
+        break;
+      case "Artifact":
+        cssColor += dark ? "bg-brown-500" : "bg-brown-200";
+        break;
+      case "Land":
+        cssColor += dark ? "bg-land-500" : "bg-land-200";
+        break;
+      case "Multicolor":
+        cssColor += dark ? "bg-gold-500" : "bg-gold-200";
+        break;
+      default:
+        cssColor += "bg-white";
+        break;
     }
     return cssColor;
-}
-export default function Card({ name, colorType, dark }: CardProps) {
-  const [background, setBackground] = useState("bg-white");
-  const { configuration } = useContext(
-    ConfigurationContext
-  ) as ConfigurationContextType;
+  }
+  export default function Card({ name, colorType, dark }: CardProps) {
+    const [background, setBackground] = useState("bg-white");
+    const { configuration } = useContext(
+      ConfigurationContext
+    ) as ConfigurationContextType;
 
-  useEffect(() => {
-    if (configuration.showColors) {
-      const cssColor = getCSSColorByType(colorType, dark);
-      setBackground(cssColor);
-    }
-    return () => {
-      setBackground("bg-white");
-    };
-  }, [configuration, colorType, dark, background]);
-  return (
-    <div
-      className={`${background} px-2 py-2 border-black border-b-0 border w-1/3 m-auto h-1/3`}
-      data-testid="card"
-    >
-      <p>{name}</p>
-    </div>
-  );
-}
+    useEffect(() => {
+      if (configuration.showColors) {
+        const cssColor = getCSSColorByType(colorType, dark);
+        setBackground(cssColor);
+      }
+      return () => {
+        setBackground("bg-white");
+      };
+    }, [configuration, colorType, dark, background]);
+    return (
+      <div
+        className={`${background} px-2 py-2 border-black border-b-0 border w-1/3 m-auto h-1/3`}
+        data-testid="card"
+      >
+        <p>{name}</p>
+      </div>
+    );
+  }
